d7f14c744088958a6f7e6036d9410f3bd4d4aad6
Marie Annonciate Hatungimana <marieannonciate.hatungimana@dci.education>
2023-02-21 09:26:56 +0000

    Adds new methods to ProductService - 2.

--- app.js
+++ app.js
@@ -9,10 +9,11 @@
  *  Product class, um Produkte in meinem Shop zu modellieren.
  */
 class Product {
-    constructor(name, price, category, isReduced) {
+    constructor(name, price, category, quantityInStock, isReduced) {
         this.name = name; // Name
         this.price = price; // Preis
         this.category = category; // Kategorie (z.B. Büro, Smartphone, TV und Audio,...)
+        this.quantityInStock = quantityInStock;
         this.isReduced = isReduced; // Im Angebot?
     }
 }
@@ -23,20 +24,23 @@ const myProducts = [
         "CANON EOS 2000D Kit Spiegelreflexkamera",
         469.99,
         "Foto and Video",
+        100,
         false
     ),
     new Product(
         "Böttcher-AG Ordner, A4, breit, 80mm, Karton, schwarz, Wolkenmarmor",
         3.25,
         "Office",
+        2,
         true
     ),
-    new Product("Samsung Galaxy S22 5G", 129.9, "Smartphones", false),
-    new Product("Minecraft: Nintendo Switch Edition", 23.99, "Games", true),
+    new Product("Samsung Galaxy S22 5G", 129.9, "Smartphones", 15, false),
+    new Product("Minecraft: Nintendo Switch Edition", 23.99, "Games", 40, true),
     new Product(
         "Sony HDC-5500 High performance",
         4895.75,
         "Foto and Video",
+        9,
         false
     ),
 ];
@@ -52,6 +56,7 @@ class ProductService {
 
     // Produkte zeigen (Verwendung von Array.forEach).
     dispayProducts(products) {
+        // Use "Auf Lager" or "Nur noch ?? Artikel im Lager" if quantityInStock < 10.
         console.log("Dispay will be done here");
     }
 
